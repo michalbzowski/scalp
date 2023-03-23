44434b78e2c1c257dcc7d042fb47f073d211ed2e
Michał Bzowski <michal.bzowski@ing.com>
2023-03-23 13:38:54 +0100

    First working templates

--- src/main/java/pl/bzowski/GreetingResource.java
+++ src/main/java/pl/bzowski/GreetingResource.java
@@ -1,5 +1,8 @@
 package pl.bzowski;
 
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
 import pro.xstore.api.message.error.APICommandConstructionException;
 import pro.xstore.api.message.error.APICommunicationException;
 import pro.xstore.api.message.error.APIReplyParseException;
@@ -9,6 +12,7 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
@@ -16,12 +20,19 @@ import java.io.IOException;
 @Path("/hello")
 public class GreetingResource {
 
-    @Inject
-    SymbolsService symbolsService;
+    @CheckedTemplate
+    public static class Templates {
+        public static native TemplateInstance hello(String name);
+    }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response hello() throws APIErrorResponse, APICommunicationException, APIReplyParseException, APICommandConstructionException, IOException {
-        return Response.ok(symbolsService.getAllCurrencyPairs()).build();
+    @Produces(MediaType.TEXT_PLAIN)
+    public TemplateInstance get(@QueryParam("name") String name) {
+        return Templates.hello(name);
     }
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response hello() throws APIErrorResponse, APICommunicationException, APIReplyParseException, APICommandConstructionException, IOException {
+//        return Response.ok(symbolsService.getAllCurrencyPairs()).build();
+//    }
 }
