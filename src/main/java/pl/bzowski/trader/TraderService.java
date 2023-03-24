beae4481a8c9d149c795f9a2a3840a015f0e68ff
Michał Bzowski <michal.bzowski@ing.com>
2023-03-24 10:34:33 +0100

    Bot impl - code compiles - to test

--- src/main/java/pl/bzowski/trader/TraderService.java
+++ src/main/java/pl/bzowski/trader/TraderService.java
@@ -1,11 +1,18 @@
 package pl.bzowski.trader;
 
 import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.bzowski.ConnectorProvider;
 import pl.bzowski.tradingbot.BotService;
 import pl.bzowski.tradingbot.TradingBot;
 import pl.bzowski.tradingbot.strategies.SimpleSarEma200Strategy;
 import pl.bzowski.tradingbot.strategies.StrategyBuilder;
 import pro.xstore.api.message.error.APICommunicationException;
+import pro.xstore.api.message.records.SCandleRecord;
+import pro.xstore.api.streaming.StreamingListener;
+import pro.xstore.api.sync.StreamingConnector;
+import pro.xstore.api.sync.SyncAPIConnector;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -15,29 +22,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Singleton
-public class TraderService {
+public class TraderService extends StreamingListener {
 
+    private static final Logger logger = LoggerFactory.getLogger(TraderService.class);
     @Inject
     BotService botService;
     @Inject
     Vertx vertx;
+
+    @Inject
+    ConnectorProvider connectorProvider;
 
     Map<String, TradingBot> activeBots = new HashMap<>();
 
     @PostConstruct
     public void startObserveMarket() {
         var executor = vertx.createSharedWorkerExecutor("my-worker", 1);
+        var syncAPIConnector = connectorProvider.get();
         executor.<String>executeBlocking(promise -> {
-            TradeBotStreamListener tradeBotStreamListener = new TradeBotStreamListener(strategies, minuteSeriesHandler);
             try {
-                connector.connectStream(tradeBotStreamListener);
-                strategies.forEach((key, value) -> {
+                syncAPIConnector.connectStream(this);
+                activeBots.keySet().forEach(symbol -> {
                     try {
-                        connector.subscribeCandle(key);
+                        syncAPIConnector.subscribeCandle(symbol);
                     } catch (APICommunicationException e) {
-                        logger.error(e.getLocalizedMessage());
+                        throw new RuntimeException(e);
                     }
-
                 });
             } catch (IOException | APICommunicationException e) {
                 logger.error(e.getLocalizedMessage());
@@ -45,10 +55,22 @@ public class TraderService {
         });
     }
 
+    @Override
+    public void receiveCandleRecord(SCandleRecord candleRecord) {
+        activeBots.values()
+                .forEach(ab -> ab.onTick(candleRecord));
+    }
+
     public void startTrade(String symbol, String strategyName) {
         StrategyBuilder strategyBuilder = getStrategyBuilder(symbol, strategyName);
         var botInstance = botService.createBotInstance(symbol, strategyBuilder);
         activeBots.put(symbol, botInstance);
+        var syncAPIConnector = connectorProvider.get();
+        try {
+            syncAPIConnector.subscribeCandle(symbol);
+        } catch (APICommunicationException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private StrategyBuilder getStrategyBuilder(String symbol, String strategyName) {
--- src/main/java/pl/bzowski/tradingbot/TradingBot.java
+++ src/main/java/pl/bzowski/tradingbot/TradingBot.java
@@ -1,7 +1,85 @@
 package pl.bzowski.tradingbot;
 
-import pro.xstore.api.streaming.StreamingListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.ta4j.core.Bar;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.BaseBar;
+import org.ta4j.core.num.DecimalNum;
+import pl.bzowski.tradingbot.strategies.StrategyWithLifeCycle;
+import pro.xstore.api.message.codes.PERIOD_CODE;
+import pro.xstore.api.message.records.SCandleRecord;
 
-public abstract class TradingBot  extends StreamingListener {
+import java.time.*;
+
+public abstract class TradingBot {
+    private static final long MINUTE_IN_MILLISECONDS = 60_000L;
+    private static final Logger logger = LoggerFactory.getLogger(TradingBot.class);
+
+    private final BarSeries series;
+    private final PERIOD_CODE periodCode;
+
+    protected final String symbol;
+    protected final StrategyWithLifeCycle longStrategy;
+    protected final StrategyWithLifeCycle shortStrategy;
+
+    public TradingBot(String symbol, BarSeries series, PERIOD_CODE periodCode, StrategyWithLifeCycle longStrategy, StrategyWithLifeCycle shortStrategy) {
+        this.symbol = symbol;
+        this.series = series;
+        this.periodCode = periodCode;
+        this.longStrategy = longStrategy;
+        this.shortStrategy = shortStrategy;
+    }
+
+    protected int updateSeriesWithOneMinuteCandle(SCandleRecord record) {
+        //Ostatni bar z serii może być jeszcze w trakcie rysowania
+        Bar lastBar = series.getLastBar();
+
+        //jeśli tak jest to jego czas początkowy plus wybrany okres powinien być większy niż czas rekordu, który wpadł w tej chwili
+
+        long ctm = record.getCtm();//Candle start time in CET time zone (Central European Time)
+        long periodDurationInMilliseconds = periodCode.getCode() * MINUTE_IN_MILLISECONDS;
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(ctm),
+                ZoneId.systemDefault());
+        ZonedDateTime currentOneMinuteBarBeginTime = localDateTime.atZone(ZoneId.systemDefault());
+        BaseBar newBar = getBaseBar(PERIOD_CODE.PERIOD_M1.getCode(), ctm, record.getClose(), record.getOpen(), record.getHigh(), record.getLow());//Zawsze jednominutwa, bo taki tik dostajemy z appi
+
+        ZonedDateTime lastBarBeginTime = lastBar.getBeginTime();
+        ZonedDateTime plus = lastBarBeginTime.plus(Duration.ofMillis(periodDurationInMilliseconds));
+        boolean isEquals = plus.isEqual(currentOneMinuteBarBeginTime);
+        if (!isEquals) {
+            series.addPrice(newBar.getClosePrice());
+            logger.info("Series updated by another one minute candle");
+            return -1; //It means that there is no new bar
+        } else {
+            logger.info("New bar added");
+            series.addBar(newBar);
+        }
+
+        int endIndex = series.getEndIndex();
+        return endIndex;
+    }
+
+    private BaseBar getBaseBar(long code, long ctm, double close, double open, double high, double low) {
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(ctm + code * 60_000),
+                ZoneId.systemDefault());
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.systemDefault());
+        return BaseBar.builder()
+                .closePrice(DecimalNum.valueOf(close))
+                .openPrice(DecimalNum.valueOf(open))
+                .highPrice(DecimalNum.valueOf(high))
+                .lowPrice(DecimalNum.valueOf(low))
+                .endTime(zonedDateTime)
+                .timePeriod(Duration.ofMinutes(code))
+                .build();
+    }
+
+    protected void logMemoryUsage() {
+        Runtime rt = Runtime.getRuntime();
+        long usedMB = (rt.totalMemory() - rt.freeMemory()) / 1024 / 1024;
+        logger.info("Memory usage {} MB", usedMB);
+    }
+
+    public abstract void onTick(SCandleRecord candleRecord);
 
 }
